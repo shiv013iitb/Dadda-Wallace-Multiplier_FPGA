c99390b75c4533c05d26fcbfd27ee2959fd65d90
Shivajee Tiwari <99135154+shiv013iitb@users.noreply.github.com>
2023-08-18 02:14:17 +0530

    Add files via upload

---
+++ DUT.vhdl
@@ -0,0 +1,28 @@
+-- A DUT entity is used to wrap your design so that we can combine it with testbench.
+-- This example shows how you can do this for the OR Gate
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity DUT is
+    port(input_vector: in std_logic_vector(15 downto 0);
+       	output_vector: out std_logic_vector(15 downto 0));
+end entity;
+
+architecture DutWrap of DUT is
+   component Dadda_Multiplication is
+     port(A, B: in std_logic_vector(7 downto 0);
+         P: out std_logic_vector(15 downto 0));
+   end component;
+begin
+
+   -- input/output vector element ordering is critical,
+   -- and must match the ordering in the trace file!
+   Mul_instance: Dadda_Multiplication 
+			port map (
+					-- order of inputs B A
+					B => input_vector(15 downto 8),
+					A => input_vector(7 downto 0),
+               -- order of output OUTPUT
+					P => output_vector(15 downto 0));
+end DutWrap;
